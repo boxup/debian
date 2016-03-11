2e33cd01586a7c931cd870be0c78611e2fa2884b
Open BaseBox.org <baseboxorg@users.noreply.github.com>
2016-03-11 21:57:53 +0800

    Update custom-script.sh

--- custom-script.sh
+++ custom-script.sh
@@ -6,4 +6,14 @@ set -eux
 # to add some additional commands for your environment
 #
 # For example:
-# apt-get install -y curl wget git 
+
+apt-get install -y cachefilesd
+
+
+echo "Setting Timezone & Locale to KL & en_US.UTF-8"
+ln -sf /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
+echo 'LC_ALL="en_US.UTF-8"' >> /etc/environment
+echo 'LANG="en_US.UTF-8"
+LC_ALL="en_US.UTF-8"' >> /etc/default/locale
+
+echo "RUN=yes" > /etc/default/cachefilesd
