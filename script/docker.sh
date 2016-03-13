aa7ddcefee272b9912f26beb0d743699884a419a
Open BaseBox.org <baseboxorg@users.noreply.github.com>
2016-03-13 23:52:15 +0800

    Update docker.sh

--- script/docker.sh
+++ script/docker.sh
@@ -19,7 +19,7 @@ docker_package_install() {
     curl -sSL https://get.docker.com/ | sh
 
     # Enable memory and swap accounting
-    sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/' /etc/default/grub
+    sed -i 's/GRUB_CMDLINE_LINUX="debian-installer=en_US"/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/' /etc/default/grub
     update-grub
 
     # Docker package does not current configure daemon to start on boot
@@ -34,46 +34,6 @@ docker_package_install() {
     sleep 60
 }
 
-docker_io_install() {
-    echo "==> Installing Docker"
-    
-    # Update sources
-    apt-get update
-    apt-get install -y docker.io
-
-    # Create /usr/bin/docker the Debian/Ubuntu way
-    # (avoid conflicting with docker – System tray)
-    update-alternatives --install /usr/bin/docker docker /usr/bin/docker.io 50
-
-    # Allow bash completion for docker
-    cp -a /etc/bash_completion.d/docker{.io,}
-    sed -i 's/\(docker\)\.io/\1/g' /etc/bash_completion.d/docker
-
-    # Allow zsh completion for docker
-    cp -a /usr/share/zsh/vendor-completions/_docker{.io,}
-    sed -i 's/\(docker\)\.io/\1/g' /usr/share/zsh/vendor-completions/_docker
-
-    # the man page for docker
-    ln -s /usr/share/man/man1/docker{.io,}.1.gz
-
-    # not really needed because docker.io is still there
-    sed -i 's/\(docker\)\.io/\1/g' /usr/share/docker.io/contrib/*.sh
-    
-    # Enable memory and swap accounting
-    sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/' /etc/default/grub
-    update-grub
-}
-
-docker_install() {
-    echo "==> Installing Docker from the Docker repository"
-
-    curl -s https://get.docker.io/ubuntu/ | sudo sh
-
-    # Enable memory and swap accounting
-    sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/' /etc/default/grub
-    update-grub
-}
-
 give_docker_non_root_access() {
     # Add the docker group if it doesn't already exist
     groupadd docker
@@ -84,4 +44,4 @@ give_docker_non_root_access() {
 }
 
 give_docker_non_root_access
-docker_package_install 
+docker_package_install
